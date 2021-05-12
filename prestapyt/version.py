38ddd301b4d919bad40336cf34cb48f4f59e613d
Julien Laloux <julien.laloux@jlaloux.be>
2021-05-12 09:27:54 +0200

    import importlib.metadata when python is less than 3.9

--- prestapyt/version.py
+++ prestapyt/version.py
@@ -1,4 +1,7 @@
-from importlib.metadata import version, PackageNotFoundError
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
 
 
 __author__ = "Guewen Baconnier <guewen.baconnier@gmail.com>"
