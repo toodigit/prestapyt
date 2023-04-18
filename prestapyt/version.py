284f7f002d3411245f8da595f64735ea1cdb9cc4
Damien Carlier <damien.carlier@gmail.com>
2023-04-18 10:43:21 +0200

    [FIX] nomodulefounderror importlib if python <3.8

--- prestapyt/version.py
+++ prestapyt/version.py
@@ -1,6 +1,7 @@
 try:
     from importlib.metadata import version, PackageNotFoundError
-except ImportError:
+except:
+    # for python < 3.8
     from importlib_metadata import version, PackageNotFoundError
 
 
